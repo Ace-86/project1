e5dd566c40a8a5961d6803095d0c9e091c402071
Ace <kronicrespawn@gmail.com>
2023-04-29 18:19:48 -0500

    router and routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Navbar from "./components/NavBar";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello </h1>
-    </div>
+    <>
+    <Router>
+     <Navbar />
+     <Routes>
+       <Route path='/' exact />
+     </Routes>
+    </Router>
+    </>
   );
 }
 
