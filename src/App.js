1bf6defa53bcc05865b3c53104366dc095eb9834
Ace <kronicrespawn@gmail.com>
2023-05-01 11:50:14 -0500

    route paths added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./components/NavBar";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/pages/Home'
 import React from 'react';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import Products from './components/pages/Products';
 
 
 
@@ -13,6 +16,9 @@ function App() {
      <Navbar />
      <Routes>
        <Route path='/' element={<Home />} />
+       <Route path='/services' element={<Services />} />
+       <Route path='/products' element={<Products />} />
+       <Route path='/sign-up' element={<SignUp />} />
      </Routes>
     </Router>
     </>
