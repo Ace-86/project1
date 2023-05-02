d49243629f5e22f5b3894c46d32e3f2551a3cc03
Ace <kronicrespawn@gmail.com>
2023-05-02 09:02:07 -0500

    dummy content added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import './Cards.css';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Random words go here and make it EPIC!</h1> 
+      <h1>Random words go here!</h1> 
       <div className='cards__container'>
         <div className='cards__wrapper'>
             <ul className='cards__items'>
